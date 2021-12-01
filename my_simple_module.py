c952491c3840d769d672f5a65e3347b9e99136c8
Metin Senturk <metinsenturk@me.com>
2021-11-30 20:20:05 -0500

    code and notebook examples

---
+++ my_simple_module.py
@@ -0,0 +1,16 @@
+print('Module started')
+
+def sum(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+def print_message(msg):
+    print('You mesage is', msg)
+
+result = sum(2, 3)
+print(result)
+
+
+print('Module finished')
